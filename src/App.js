9c7c372b613bb315d580175b93412181ab28f32d
akshay <akshay@jivrus.com>
2021-08-05 12:54:00 +0530

    Adding code on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Billing from './component/Billing';
+import Cart from './component/Cart';
+import Payment from './component/Payment';
+
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <header className="App-Header">
+      <Billing />
+      <Cart />
+      <Payment />
       </header>
     </div>
+    
+    
   );
 }
 
---
+++ src/component/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+const Cart = (props) =>{
+    return(
+        <div Container>
+            <div className="col-4">
+            </div>
+        </div>
+    )
+}
+
+export default Cart;
